c4f7f5750ef818d29b33bdeb7d480fee7d6f5fcf
poghosyanmariam44 <poghosyanmariam44@gmail.com>
2025-04-04 14:44:11 +0400

    Add chioces

---
+++ bmt/assignment/choices.py
@@ -0,0 +1,6 @@
+ASSIGNMENT_STATUSES = [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+    ('overdue', 'Overdue'),
+]
--- bmt/assignment/models.py
+++ bmt/assignment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .choices import ASSIGNMENT_STATUSES
 
 class Assignment(models.Model):
     title = models.CharField(max_length=100)
@@ -8,5 +9,8 @@ class Assignment(models.Model):
     business = models.CharField(max_length=500)
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
     due_date = models.DateField()
-    status = models.CharField(max_length=500)
+    status = models.CharField(max_length=500, choices=ASSIGNMENT_STATUSES)
     uploaded_by = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
