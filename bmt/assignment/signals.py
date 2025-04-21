ac9a06705978d99196b2b7de07a92fba598c55c7
poghosyanmariam44 <poghosyanmariam44@gmail.com>
2025-04-21 13:29:36 +0400

    sending email

--- bmt/assignment/signals.py
+++ bmt/assignment/signals.py
@@ -1,24 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db import transaction
 from .models import Assignment
 from .tasks import send_assignment_email
 
 @receiver(post_save, sender=Assignment)
 def notify_user_on_assignment_create(sender, instance, created, **kwargs):
     if created:
-        def send_email_after_commit():
-            user = instance.assigned_to
-            subject = f"New Assignment: {instance.title}"
-            message = (
-                f"Hi {user.username},\n\n"
-                f"You’ve been assigned a new task:\n\n"
-                f"{instance.description}\n\n"
-                f"Due: {instance.due_date}"
-            )
+        user = instance.assigned_to
+        subject = f"New Assignment: {instance.title}"
+        message = (
+            f"Hi {user.username},\n\n"
+            f"You’ve been assigned a new task:\n\n"
+            f"{instance.description}\n\n"
+            f"Due: {instance.due_date}"
+        )
 
-            # Celery task
-            send_assignment_email.delay(user.email, subject, message)
-
-        transaction.on_commit(send_email_after_commit)
-
+        # Celery task
+        send_assignment_email.delay(user.email, subject, message)
+    # pass
--- bmt/assignment/tasks.py
+++ bmt/assignment/tasks.py
@@ -1,22 +1,6 @@
-# from celery import shared_task
-# import smtplib, ssl
-# import os
-#
-# @shared_task
-# def send_assignment_email(receiver_email, subject, body):
-#     sender_email = "tinn.marutyan.04@gmail.com"
-#     password = "your real password here"  # use an env var!
-#
-#     message = f"Subject: {subject}\n\n{body}"
-#     context = ssl._create_unverified_context()
-#
-#     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-#         server.login(sender_email, password)
-#         server.sendmail(sender_email, receiver_email, message)
-
-
 from celery import shared_task
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 
 @shared_task(name='my_task')
@@ -35,7 +19,6 @@ def my_task2(arg1, arg2):
 def my_task3(request):
     return HttpResponse("The task was called successfully!")
 
-from django.core.mail import send_mail
 
 from celery import shared_task
 from django.core.mail import send_mail
