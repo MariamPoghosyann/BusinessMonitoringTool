789ea5a086c9e046e681455a8beac9e4087f96a2
poghosyanmariam44 <poghosyanmariam44@gmail.com>
2025-05-02 16:09:58 +0400

    modified email

--- bmt/assignment/tasks.py
+++ bmt/assignment/tasks.py
@@ -5,10 +5,9 @@ from django.utils.html import strip_tags
 
 
 @shared_task(name='send_assignment_email')
-def send_assignment_email(to_email, subject, context):
-    html_content = render_to_string('email.html', context)
+def send_assignment_email(to_email, subject, context, template_name):
+    html_content = render_to_string(template_name, context)
 
-    # Generate plain text version by stripping the HTML
     text_content = strip_tags(html_content)
 
     email = EmailMultiAlternatives(
@@ -20,18 +19,3 @@ def send_assignment_email(to_email, subject, context):
     email.attach_alternative(html_content, "text/html")
     email.send()
 
-# @shared_task(name='my_task')
-# def my_task(arg1, arg2):
-#     # Task logic here
-#     result = arg1 + arg2
-#     return result
-#
-#
-# @shared_task(name='my_task2')
-# def my_task2(arg1, arg2):
-#     result = arg1 - arg2
-#     return result
-#
-# @shared_task(name='my_task3')
-# def my_task3(request):
-#     return HttpResponse("The task was called successfully!")
